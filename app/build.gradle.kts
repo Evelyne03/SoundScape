3a36115d87e57768f5a651b760048c48b7c33840
Tudureanu Evelyne-Andreea <evelyne.tudureanu03@e-uvt.ro>
2024-06-21 22:51:10 +0300

    Firebase gradle implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -106,6 +106,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.ui.android)
+    implementation(libs.firebase.database.ktx)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
